9a1d652e836e3b9ac16e3ae412d58e7352f17b95
kinza-khan <kinzaaptech@gmail.com>
2025-09-18 15:33:17 +0500

    navbar

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,10 +1,11 @@
 import { Component ,OnInit  } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-   imports: [RouterLink], 
+   imports: [RouterLink,RouterModule], 
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
